b85298b22cfcbbba1836bd12a66dc097ee0a7302
kuma4188 <psb4188@gmail.com>
2024-10-09 16:31:00 +0900

    완료

--- src/main/java/challenging/application/Application.java
+++ src/main/java/challenging/application/Application.java
@@ -2,7 +2,12 @@ package challenging.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = {"challenging.application.auth.domain",
+	"challenging.application.challenge.domain",
+	"challenging.application.domain",
+	"challenging.application.userprofile.domain"})
 @SpringBootApplication
 public class Application {
 
